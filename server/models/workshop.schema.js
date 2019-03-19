202e1e2c244df3cf55715243ecc7157b8ad791d4
Umashankar Sivakumar <umashankarsivakumar@gmail.com>
2019-03-19 23:01:09 +0530

    Added workshop endpoints

--- server/models/workshop.schema.js
+++ server/models/workshop.schema.js
@@ -6,10 +6,27 @@ let workshopRegistration = new Schema ({
         name : String,
         usn : String,
         branch : String, 
+        email : String,
         semester : Number
     },
     workshopName : String,
+    workshopId : Object,
     timeStamp : Date
 })
 
-module.exports = mongoose.model('workshopRegistration', workshopRegistration)
+let workshopDetails = new Schema({
+    workshop :{
+        name : String,
+        description : String,
+        organizers : Array,
+        requirements : Array
+    },
+    timeStamp: Date,
+    workshopOn : Date,
+    hours : Number,
+})
+
+const WorkshopRegistration = mongoose.model('workshopRegistration', workshopRegistration)
+const WorkshopDetails = mongoose.model('workshopDetails',workshopDetails)
+
+module.exports = {WorkshopDetails,WorkshopRegistration}
--- server/routes/workshop.js
+++ server/routes/workshop.js
@@ -1,26 +1,45 @@
-const workshopRegistration = require('../models/workshop.schema')
+const {WorkshopRegistration,WorkshopDetails} = require('../models/workshop.schema')
+
+
 
 module.exports = (Routes) =>{
-    // endpoint to retrieve details from db
-    Routes.route('/workshopRegistrationDetails').get(function(req, res) {
-        workshopRegistration.find(function(err, body) {
+
+    // endpoint to retrieve workshop registration details from db
+    Routes.route('/workshop/registrationDetails').get((req, res) => {
+        WorkshopRegistration.find(function(err, body) {
             if (err) {
                 console.log(err);
             } else {
                 res.json(body);
             }
-        });
-    });
+        })
+    })
 
-    //endpoint to push details into db
-    Routes.route('/registrationWorkshop').post(function(req, res) {
-        let todo = new workshopRegistration(req.body);
-        todo.save()
+    //endpoint to push workshop registration details into db
+    Routes.route('/workshop/registration').post((req, res) => {
+        var body = req.body
+        body['timeStamp'] = new Date()
+        let workshopRegistration = new WorkshopRegistration(body);
+        workshopRegistration.save()
             .then(registration => {
                 res.status(200).json({'registration': 'registration added successfully'});
             })
             .catch(err => {
                 res.status(400).send('registration failed');
-            });
-    });
+            })
+    })
+
+    // endpoint to push workshop detials into db
+    Routes.route('/workshop/add').post((req, res) => {
+        var body = req.body
+        body['timeStamp'] = new Date()
+        let workshop = new WorkshopDetails(body)
+        workshop.save()
+            .then((result) => {
+                res.status(200).send('Inserted successfully with \n'+JSON.stringify(result))
+            })
+            .catch((error) => {
+                res.status(400).send(error)
+            })
+    })
 }
